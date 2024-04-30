280b771927d41d0e7963c2bb8b8520d699934b99
bivek.joshi <bivek.joshi@dghub.io>
2024-04-30 12:55:36 +0545

    changes

--- src/page/Quote/Form/RequestForm.jsx
+++ src/page/Quote/Form/RequestForm.jsx
@@ -1,9 +1,55 @@
+import { Button, Paper, TextField } from '@mui/material';
 import React from 'react';
 
 const RequestForm = () => {
   return (
-    <div>
-      
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <Paper elevation={3} style={{ padding: "2rem", width: "50%",display:"flex",flexDirection:"column", gap:"1rem" }}>
+        <div style={{ color: "#4273C1", fontWeight: 900, fontSize: "25px", marginBottom: "1rem" }}>Request Form</div>
+        <TextField
+          label="Full Name"
+          fullWidth
+          size="small"
+          InputLabelProps={{
+            shrink: false,
+          }}
+        />
+        <TextField
+          label="Email Address"
+          fullWidth
+          size="small"
+          InputLabelProps={{
+            shrink: false,
+          }}
+        />
+        <TextField
+          label="Contact Number"
+          fullWidth
+          size="small"
+          InputLabelProps={{
+            shrink: false,
+          }}
+        />
+        <TextField
+          label="Service title"
+          fullWidth
+          size="small"
+          InputLabelProps={{
+            shrink: false,
+          }}
+        />
+        <TextField
+          label="Service Description"
+          fullWidth
+          multiline
+          rows={4}
+          InputLabelProps={{
+            shrink: false,
+          }}
+          sx={{ '& input': { fontSize: '8px' } }}
+        />
+        <Button variant='contained' style={{ marginTop: "1rem" }}>Submit</Button>
+      </Paper>
     </div>
   );
 }
--- src/page/Quote/Quote/Quote.jsx
+++ src/page/Quote/Quote/Quote.jsx
@@ -1,6 +1,7 @@
-import { Button, TextField } from '@mui/material';
+import { Button, Grid, TextField,Box } from '@mui/material';
 import React from 'react';
 import Image from "../../../assets/images/p5.jpg"
+import RequestForm from '../Form/RequestForm';
 const Quote = () => {
     return (
         <div>
@@ -9,8 +10,8 @@ const Quote = () => {
                 <div style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 255, 0.4)" }}></div>
                 <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", textAlign: "center", color: "white" }}>
                     {/* <div style={{ display: "flex", flexDirection: "row", gap: "1rem"}}> */}
-                        <div>Have some work for us?</div>
-                        {/* <div style={{ width: "60px", height: "2px", backgroundColor: "#34D6FF", borderRadius: "1px" }}></div> */}
+                    <div>Have some work for us?</div>
+                    {/* <div style={{ width: "60px", height: "2px", backgroundColor: "#34D6FF", borderRadius: "1px" }}></div> */}
                     {/* </div> */}
                     <h2>Request A Quote Today</h2>
                     <p>Please Contact us with all your water needs. We look forward to servicing you.</p>
@@ -64,6 +65,31 @@ const Quote = () => {
                     <Button variant='contained'>Submit</Button>
                 </div>
             </div> */}
+            <Box sx={{padding:"22px",width:"100%"}}>
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid item xs={12} md={6}>
+                    <RequestForm />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "1rem", color: "#5D79E2", textAlign: "center" }}>
+                        <div>Visit Our Office</div>
+                        <div style={{ width: "60px", height: "2px", backgroundColor: "#34D6FF", borderRadius: "1px" }}></div>
+                    </div>
+                    <div style={{ color: "#4273C1", fontWeight: 900, fontSize: "35px", padding: "1px 0px" }}>Our Main Office</div>
+                    <div>We are welcome to visit our office during office hours, Find <br /> details of our location and business hours below.</div>
+                    <div style={{ width: "160px", height: "2px", backgroundColor: "grey", borderRadius: "1px", margin: "1rem 0rem" }}></div>
+                    <div style={{ marginTop: "1rem" }}>Address: <span style={{ color: "grey", marginLeft: "1rem" }}>LaPorte,IN 4567</span></div>
+                    <div style={{ marginTop: "1rem" }}>Phone: <span style={{ color: "grey", marginLeft: "1rem" }}>(123) 123-1234</span></div>
+                    <div style={{ marginTop: "1rem" }}>Business Hours: <span style={{ color: "grey", marginLeft: "1rem" }}>Mon-Fri: 9 am-5pm</span></div>
+                    <div style={{ marginTop: "1rem" }}>Email Address: <span style={{ color: "grey", marginLeft: "1rem" }}>yourbusiness@business.com</span></div>
+                    <div style={{ color: "#4273C1", fontWeight: 900, fontSize: "35px", padding: "1px 0px" }}>Area Covered</div>
+                    <div style={{ marginTop: "1rem",color:"grey" }}>LaPorte countries</div>
+                    <div style={{ marginTop: "1rem",color:"grey" }}>Porter countries</div>
+                    <div style={{ marginTop: "1rem",color:"grey" }}>Michigan City</div>
+                    <div style={{ marginTop: "1rem",color:"grey" }}>Michigan</div>
+                </Grid>
+            </Grid>
+            </Box>
             {/* <div
                 style={{
                     backgroundColor: "#EFCB89",
